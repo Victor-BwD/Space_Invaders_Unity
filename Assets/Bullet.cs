2657f2d3c87188e9275bfd0c1ad57af031969102
Victor-BwD <victor.bogdanowdornelles@gmail.com>
2021-12-04 01:25:37 -0300

    [ADDED] Player fire

---
+++ Assets/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Vector3 direction;
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += direction * speed * Time.deltaTime;
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,11 @@ public class Player : MonoBehaviour
 {
     public float speed = 0.5f;
 
+    public Bullet bulletPrefab;
+
+    private float fireRate = 1.0f;
+    private float nextFire;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +20,27 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+       
+        
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             this.transform.position += Vector3.left * speed * Time.deltaTime;
         }else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             this.transform.position += Vector3.right * speed * Time.deltaTime;
         }
+
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        Instantiate(bulletPrefab, transform.position, Quaternion.identity);
     }
 }
